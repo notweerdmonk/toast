5416d755fe8a263ca35b54e92182024474141e58
notweerdmonk <wrdmnk@gmail.com>
2025-06-09 13:57:27 +0530

    Encapsulate toast-info class in toast.js

--- src/toast.js
+++ src/toast.js
@@ -21,8 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import "./toast.css";
-
 class __toaster {
 
   constructor() {
@@ -69,11 +67,111 @@ class __toaster {
       merged_options.duration = 3000;
     }
 
-    const toast = document.createElement('div');
+    const toast = document.createElement("div");
     toast.classList.add('toast', category, merged_options.position);
     toast.textContent = message;
 
-    document.body.appendChild(toast);
+    const style = document.createElement("style");
+    style.textContent = `
+      .toast {
+        position: fixed;
+        min-width: 200px;
+        padding: 10px 20px;
+        text-align: center;
+        font-size: 1rem;
+        font-weight: 400;
+        font-family: Roboto, Arial, sans-serif;
+        border: 1px solid;
+        border-radius: 0.25rem;
+        transition: opacity 0.5s ease-in-out;
+        z-index: 1000; /* Ensure toast is above other elements */
+        cursor: default;
+      }
+
+      .toast.top {
+        top: 20px;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+
+      .toast.bottom {
+        bottom: 20px;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+
+      .toast.left {
+        left: 20px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+
+      .toast.right {
+        right: 20px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+
+      .toast.top-left {
+        top: 20px;
+        left: 20px;
+      }
+
+      .toast.top-right {
+        top: 20px;
+        right: 20px;
+      }
+
+      .toast.bottom-left {
+        left: 20px;
+        bottom: 20px;
+      }
+
+      .toast.bottom-right {
+        right: 20px;
+        bottom: 20px;
+      }
+
+      .toast.dark {
+        color: #1b1e21;
+        background-color: #d6d8d9;
+        border-color: #c6c8ca;
+      }
+
+      .toast.info {
+        color: #0c5460;
+        background-color: #d1ecf1;
+        border-color: #bee5eb;
+      }
+      .toast.success {
+        color: #155724;
+        background-color: #d4edda;
+        border-color: #c3e6cb;
+      }
+
+      .toast.warning {
+        color: #856404;
+        background-color: #fff3cd;
+        border-color: #ffeeba;
+      }
+
+      .toast.error {
+        color: #721c24;
+        background-color: #f8d7da;
+        border-color: #f5c6cb;
+      }
+
+      .toast.fade-out {
+        opacity: 0;
+      }
+      `;
+
+    const shadow = toast.attachShadow({ mode: "closed" });
+    shadow.appendChild(style);
+    toast.appendChild(shadow);
+
+    const slot = document.createElement("slot");
+    shadow.appendChild(slot);
 
     setTimeout(() => {
       toast.classList.add('fade-out');
@@ -86,6 +184,8 @@ class __toaster {
     toast.addEventListener('click', () => {
       toast.remove();
     });
+
+    document.body.appendChild(toast);
   }
 
   success(message, options = {}) {
@@ -104,6 +204,70 @@ class __toaster {
     this.show_toast(message, 'warning', options);
   }
 }
+
+class __toast extends HTMLElement {
+  constructor(toaster) {
+    super();
+    this.addEventListener('click', this.display);
+    this.toaster = toaster;
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "closed" });
+
+    const style = document.createElement("style");
+    style.textContent = `
+      .::slotted(button) {
+        padding: 0;
+        margin 0;
+        border: none;
+        box-sizing: border-box;
+        width: 100%;
+        height: 100%;
+      }
+      `;
+
+    shadow.appendChild(style);
+
+    const slot = document.createElement('slot');
+    shadow.appendChild(slot);
+  }
+
+  display() {
+  }
+}
+
+class __toast_info extends __toast {
+  display() {
+    toaster.info('This is an info message!', { position: 'top-left'});
+  }
+}
+
+customElements.define('toast-info', __toast_info);
+
+class __toast_success extends __toast {
+  display() {
+    toaster.success('This is an success message!', { position: 'top-right'});
+  }
+}
+
+customElements.define('toast-success', __toast_success);
+
+class __toast_warning extends __toast {
+  display() {
+    toaster.warning('This is an warning message!', { position: 'bottom-left'});
+  }
+}
+
+customElements.define('toast-warning', __toast_warning);
+
+class __toast_error extends __toast {
+  display() {
+    toaster.error('This is an error message!', { position: 'bottom-right'});
+  }
+}
+
+customElements.define('toast-error', __toast_error);
 
 export const toaster = new __toaster();
 export default toaster;
